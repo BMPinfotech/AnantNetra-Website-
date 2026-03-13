aee2fffecb0af001ebbd52136402dba64d3975a8
Vinit-Soni16 <israniprincy@gmail.com>
2026-03-13 15:22:22 +0530

    Images change

--- frontend/src/app/about-us/OurTimeline.tsx
+++ frontend/src/app/about-us/OurTimeline.tsx
@@ -17,14 +17,14 @@ export function OurTimeline() {
           </ul>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://i.pinimg.com/736x/5c/14/72/5c14720afdd38dad4e842657263dbfc5.jpg"
+              src="/images/Bright-Modern-Office.WebP"
               alt="Bright Modern Office"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-              src="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?q=80&w=1200&auto=format&fit=crop"
+              src="/images/Strategy-Ideation.WebP"
               alt="Strategy Ideation"
               width={500}
               height={500}
@@ -46,14 +46,14 @@ export function OurTimeline() {
           </ul>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://i.pinimg.com/736x/70/52/ad/7052ad5f76a69b85133af4569959dc32.jpg"
+              src="/images/Team-Expansion-Success.WebP"
               alt="Team Expansion Success"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-              src="https://i.pinimg.com/1200x/10/bd/ce/10bdcee5acece5da5ea77a89d61ee653.jpg"
+              src="/images/Global.WebP"
               alt="Global Server Network"
               width={500}
               height={500}
@@ -75,14 +75,14 @@ export function OurTimeline() {
           </ul>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1200&auto=format&fit=crop"
+              src="/images/Strategic-Growth.WebP"
               alt="Strategic Growth"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-              src="https://images.unsplash.com/photo-1533750349088-cd871a92f312?q=80&w=1200&auto=format&fit=crop"
+              src="/images/Partnership.WebP"
               alt="Global Partnerships"
               width={500}
               height={500}
@@ -104,14 +104,14 @@ export function OurTimeline() {
           </ul>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://images.unsplash.com/photo-1677442136019-21780ecad995?q=80&w=1200&auto=format&fit=crop"
+              src="/images/AI-Tech.WebP"
               alt="AI Technology"
               width={500}
               height={500}
               className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
-              src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=1200&auto=format&fit=crop"
+              src="/images/Digital.WebP"
               alt="Digital Infrastructure"
               width={500}
               height={500}
